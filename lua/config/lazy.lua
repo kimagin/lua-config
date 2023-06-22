64d17bf208a4900d982773e7e45c381241658d28
kimagin <kimagin@KIMAGINATION-WS>
2023-06-22 13:51:14 +0200

    work environment implementation

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -70,13 +70,13 @@ bufferline.setup({
 
 require("rose-pine").setup({
   --- @usage 'auto'|'main'|'moon'|'dawn'
-  variant = "main",
+  variant = "auto",
   --- @usage 'main'|'moon'|'dawn'
   dark_variant = "main",
   -- bold_vert_split = false,
   dim_nc_background = true,
   disable_background = false,
-  disable_float_background = false,
+  disable_float_background = true,
   disable_italics = false,
 
   --- @usage string hex value or named color from rosepinetheme.com/palette
