bcdfccbdae37e5133d811853b33514284dcc02f7
kimagin <kimagin@KIMAGINATION-WS>
2023-06-24 22:45:25 +0200

    fix: broken icon of empty folder

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      popup_border_style = "rounded",
       filesystem = {
         filtered_items = {
           -- hide_dotfiles = false,
@@ -11,6 +12,11 @@ return {
           always_show = { -- remains visible even if other settings would normally hide it
             ".env",
           },
+        },
+      },
+      default_component_configs = {
+        icon = {
+          folder_empty = "󰉖",
         },
       },
     },
