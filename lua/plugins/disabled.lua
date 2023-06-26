57105c4bf5782fe9a3ac7266b0a1171d1e81add9
kimagin <info@kimagination.net>
2023-06-26 03:10:53 +0200

    cleaning up additional files

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -7,4 +7,6 @@ return {
   -- { "echasnovski/mini.indentscope", enabled = false },
   -- { "akinsho/bufferline.nvim", enabled = false },
   -- { "folke/edgy.nvim", enabled = false },
+  -- { "L3MON4D3/LuaSnip", enabled = false },
+  -- { "rafamadriz/friendly-snippets", enabled = false },
 }
