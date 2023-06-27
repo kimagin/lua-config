af882e3bc1d0158d1fbc5db396f598643b6e08be
kimagin <info@kimagination.net>
2023-06-27 07:03:02 +0200

    vertical terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,7 +7,14 @@ return {
         open_mapping = [[<c-\>]],
         shade_terminals = true,
         shading_factor = "0",
-        direction = "horizontal",
+        direction = "vertical",
+        size = function(term)
+          if term.direction == "horizontal" then
+            return 15
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          end
+        end,
         close_on_exit = true,
       },
     },
